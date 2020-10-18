4a6cae6e6efc1af60d28303e12169099ae3e6c7a
Michael-Mbaya <gikundamike@gmail.com>
2020-10-18 18:15:17 +0300

    Characters Activity

--- app/src/main/java/com/example/lotrcharacters/CharactersActivity.java
+++ app/src/main/java/com/example/lotrcharacters/CharactersActivity.java
@@ -1,15 +1,15 @@
 package com.example.lotrcharacters;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CharactersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_characters);
     }
 
 }
