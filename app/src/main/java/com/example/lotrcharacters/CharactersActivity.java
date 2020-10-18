64c89b9f6e8d3481b9bdf216557e4fec444d86af
Michael-Mbaya <gikundamike@gmail.com>
2020-10-18 18:50:05 +0300

    Intent to activities

--- app/src/main/java/com/example/lotrcharacters/CharactersActivity.java
+++ app/src/main/java/com/example/lotrcharacters/CharactersActivity.java
@@ -1,15 +1,25 @@
 package com.example.lotrcharacters;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CharactersActivity extends AppCompatActivity {
+
+    @BindView(R.id.welcomeTextView) TextView mWlcome;
+    @BindView(R.id.charactersList) ListView mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_characters);
+        ButterKnife.bind(this);
     }
 
 }
--- app/src/main/java/com/example/lotrcharacters/MainActivity.java
+++ app/src/main/java/com/example/lotrcharacters/MainActivity.java
@@ -2,14 +2,51 @@ package com.example.lotrcharacters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.nameEditText) EditText mWlcome;
+    @BindView(R.id.nextActButton) Button mNextActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mNextActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mNextActivity) {
+
+            if (mWlcome.getText().toString().isEmpty()) {
+                Toast.makeText(MainActivity.this, "Text is Required", Toast.LENGTH_LONG).show();
+                mWlcome.setError("Search Text is Required");
+                Toast.makeText(MainActivity.this, "Text is Required", Toast.LENGTH_LONG).show();
+            } else {
+                String myNameIs = mWlcome.getText().toString();
+                Intent intent = new Intent(MainActivity.this, CharactersActivity.class);
+                //pass data with intent extras
+                intent.putExtra("myName", myNameIs);
+                //go to search activity
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Welcome " + myNameIs + "!", Toast.LENGTH_LONG).show();
+            }
+
+        }
+
     }
 
 }
